e35ce9290486adb0c59c59c15bd575e1a64d0fd8
rusty015 <nailrusty.dev@gmail.com>
2024-05-26 11:29:58 +0200

    refactor output of token price and liquidity

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,15 @@ use solana_sdk::pubkey::Pubkey;
 
 fn get_pool_pda(amm_program: Pubkey, amm_pool: Pubkey, cluster_url: &str) -> Result<()> {
     let client = RpcClient::new(cluster_url.to_string());
-    // let amm_info: AmmInfo = rpc::get_account::<AmmInfo>(&client, &amm_pool)?.unwrap();
+    let amm_info: common::AmmInfo =
+        common::rpc::get_account::<common::AmmInfo>(&client, &amm_pool)?.unwrap();
+    let quote_token_decimal: u64 = amm_info.pc_decimals;
+    let quote_token_decimal_f64: f64 = quote_token_decimal as f64;
+    let base_token_decimal: u64 = amm_info.coin_decimals;
+    let base_token_decimal_f64: f64 = base_token_decimal as f64;
     let amm_keys = utils::load_amm_keys(&client, &amm_program, &amm_pool).unwrap();
     let market_keys =
-        openbooks::get_keys_for_market(&client, &amm_keys.market_program, &amm_keys.market)
-            .unwrap();
+        openbooks::get_keys_for_market(&client, &amm_keys.market_program, &amm_keys.market)?;
     let calculate_result = amm::calculate_pool_vault_amounts(
         &client,
         &amm_program,
@@ -25,8 +29,17 @@ fn get_pool_pda(amm_program: Pubkey, amm_pool: Pubkey, cluster_url: &str) -> Res
         &amm_keys,
         &market_keys,
         amm::utils::CalculateMethod::CalculateWithLoadAccount,
-    ).unwrap();
-    println!("calculate_result:{:?}", calculate_result);
+    )?;
+    let quote_token_amount: u64 = calculate_result.pool_pc_vault_amount;
+    let quote_token_amount_f64: f64 = quote_token_amount as f64;
+    let base_token_amount: u64 = calculate_result.pool_coin_vault_amount;
+    let base_token_amount_f64: f64 = base_token_amount as f64;
+    let base_toke_price: f64 = (quote_token_amount_f64 / (10_f64.powf(quote_token_decimal_f64)))
+        / (base_token_amount_f64 / (10_f64.powf(base_token_decimal_f64)));
+    let liqudity_as_quote_token: f64 =
+        quote_token_amount_f64 / (10_f64.powf(quote_token_decimal_f64)) * 2_f64;
+    println!("Base Token Price: {}", base_toke_price);
+    println!("Liquidity as quotetoken: {}", liqudity_as_quote_token);
     Ok(())
 }
 
